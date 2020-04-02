3fd0e393f429eeb37891f1150b2f0dd26c742f51
RJMW <r.j.weber@bham.ac.uk>
2020-04-02 15:59:35 +0100

    Add license header

---
+++ metaboverse/__main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019-2020 Ralf Weber
+#
+# This file is part of MetaboVerse.
+#
+# MetaboVerse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MetaboVerse is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MetaboVerse.  If not, see <https://www.gnu.org/licenses/>.
+#
