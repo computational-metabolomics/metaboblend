2523d46769cbf6a2a5f1491adcf5290c96413c00
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2020-05-20 18:24:02 +0100

    Implement dynamic programming version of subset sum

--- metaboverse/build_structures.py
+++ metaboverse/build_structures.py
@@ -32,36 +32,86 @@ from rdkit import Chem
 from .databases import SubstructureDb
 
 
-def subset_sum(l, mass, toll=0.001):
+def find_path(l, dp, n, mass, max_subset_length, path=[]):
     """
-    Recursive subset sum algorithm for identifying sets of substructures that make up a given mass; the first step
-    of building molecules from substructures.
+    Recursive solution for backtracking through the dynamic programming boolean matrix. All possible subsets are found
 
     :param l: A list of masses from which to identify subsets.
 
     :param mass: The target mass of the sum of the substructures.
 
-    :param toll: The allowable deviation of the sum of subsets from the target mass.
+    :param dp: The dynamic programming boolean matrix.
+
+    :param n: The size of l.
+
+    :param max_subset_length: The maximum length of subsets to return. Allows the recursive backtracking algorithm to
+        terminate early in many cases, significantly improving runtime.
+
+    :param path: List for keeping track of the current subset.
 
     :return: Generates of lists containing the masses of valid subsets.
     """
 
-    if mass < -toll:
+    # base case - the path has generated a correct solution
+    if mass == 0:
+        yield sorted(path)
         return
 
-    elif len(l) == 0:
-        if -toll <= mass <= toll:
-            yield []
+    # stop running when we overshoot the mass
+    elif mass < 0:
         return
 
-    elif abs(sum(l) - mass) <= toll:
-        yield l
-        return
+    # can we sum up to the target value using the remaining masses? recursive call
+    elif dp[n][mass]:
+        yield from find_path(l, dp, n-1, mass, max_subset_length, path)
 
-    for subset in subset_sum(l[1:], mass):
-        yield subset
-    for subset in subset_sum(l[1:], mass - l[0]):
-        yield [l[0]] + subset
+        if len(path) < max_subset_length:
+            path.append(l[n-1])
+
+            yield from find_path(l, dp, n-1, mass - l[n-1], max_subset_length, path)
+            path.pop()
+
+
+def subset_sum(l, mass, max_subset_length=3):
+    """
+    Dynamic programming implementation of subset sum. Note that, whilst this algorithm is pseudo-polynomial, the
+    backtracking algorithm for obtaining all possible subsets has exponential complexity and so remains unsuitable
+    for large input values.  This does, however, tend to perform a lot better than non-dp implementations, as we're
+    no longer doing sums multiple times and we've cut down the operations performed during the exponential portion of
+    the method.
+
+    :param l: A list of masses from which to identify subsets.
+
+    :param mass: The target mass of the sum of the substructures.
+
+    :param max_subset_length: The maximum length of subsets to return. Allows the recursive backtracking algorithm to
+        terminate early in many cases, significantly improving runtime.
+
+    :return: Generates of lists containing the masses of valid subsets.
+    """
+    n = len(l)
+
+    # initialise dynamic programming array
+    dp = numpy.ndarray([n+1, mass+1], bool)
+
+    # subsets can always equal 0
+    for i in range(n+1):
+        dp[i][0] = True
+
+    # empty subsets do not have non-zero sums
+    for i in range(mass):
+        dp[0][i+1] = False
+
+    # fill in the remaining boolean matrix
+    for i in range(n):
+        for j in range(mass+1):
+            if j >= l[i]:
+                dp[i+1][j] = dp[i][j] or dp[i][j-l[i]]
+            else:
+                dp[i+1][j] = dp[i][j]
+
+    # backtrack through the matrix recursively to obtain all solutions
+    return find_path(l, dp, n, mass, max_subset_length)
 
 
 def combine_ecs(ss2_grp, db, table_name, accuracy, ppm=None):
