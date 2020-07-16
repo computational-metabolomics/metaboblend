8cf9419a80d77636a97da676f71f8b00fb9a95a2
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2020-07-16 16:49:19 +0100

    Build substructures for each set of masses independently

--- metaboblend/build_structures.py
+++ metaboblend/build_structures.py
@@ -407,7 +407,6 @@ def build(mc, exact_mass, fn_out, heavy_atoms, max_valence, accuracy, max_atoms_
               """.format(exact_mass__1, len(mass_values), len(subsets)))
         print("------------------------------------------------------")
 
-    lls = []
     for ss_grp in subsets:
         if len(ss_grp) > max_n_substructures or len(ss_grp) == 0:
             continue
@@ -432,14 +431,15 @@ def build(mc, exact_mass, fn_out, heavy_atoms, max_valence, accuracy, max_atoms_
                   """.format(exact_mass__0_0001, len(mass_values_r2), len(subsets_r2)))
             print("------------------------------------------------------")
 
+        lls = []
         for ss_grp2 in subsets_r2:  # refines groups based on ecs and gets substructures from db (appends to lls)
             build_from_subsets(ss_grp2, mc=mc, table_name=table_name, ppm=ppm, debug=debug, db=db, lls=lls)
 
-    with multiprocessing.Pool(processes=processes) as pool:  # send sets of substructures for building
-        smi_list = pool.map(partial(lll_build, debug=debug, configs_iso=configs_iso), lls)
+        with multiprocessing.Pool(processes=processes) as pool:  # send sets of substructures for building
+            smi_list = pool.map(partial(lll_build, debug=debug, configs_iso=configs_iso), lls)
 
-    if len(smi_list) != 0:
-        out.writelines(smi_list)
+        if len(smi_list) != 0:
+            out.writelines(smi_list)
 
     out.close()
     db.close()
