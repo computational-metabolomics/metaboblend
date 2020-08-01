2293f8eda2cf85f77f5cc5e33d4cf2ede4a21d7b
Jack Gisby <43494691+jackgisby@users.noreply.github.com>
2020-08-01 22:22:26 +0100

    Bring scripts up to date with variable renaming

--- scripts/run.py
+++ scripts/run.py
@@ -2,9 +2,7 @@ from metaboblend.databases import *
 from metaboblend.build_structures import *
 
 
-def build_graph_isomorphism_database(sizes=[1, 2], boxes=3,
-                                     db_out='../databases/k_graphs.sqlite',
-                                     pkls_out='../databases/pkls', path=".."):
+def build_graph_isomorphism_database(sizes=[1, 2], boxes=3, db_out='../databases/k_graphs.sqlite'):
     #########################################
     # BUILD GRAPH ISOMORPHISM DATABASE
     #########################################
@@ -13,51 +11,33 @@ def build_graph_isomorphism_database(sizes=[1, 2], boxes=3,
     # http://pallini.di.uniroma1.it/
 
     if sys.platform == "win32" or sys.platform == "win64":
-        path_ri = os.path.join(path, "tools", "RI_win", "RI3.6-release", "ri36")
+        path_ri = os.path.join("..", "tools", "RI_win", "RI3.6-release", "ri36")
 
     elif sys.platform == "darwin":
-        path_ri = os.path.join(path, "tools", "RI_mac", "RI3.6-release", "ri36")
+        path_ri = os.path.join("..", "tools", "RI_mac", "RI3.6-release", "ri36")
 
     elif sys.platform == "linux2":
         if "bb" in "socket.gethostname":
-            path_ri = os.path.join(path, "tools", "RI_unix", "RI3.6-release", "ri36")
+            path_ri = os.path.join("..", "tools", "RI_unix", "RI3.6-release", "ri36")
         else:
-            path_ri = os.path.join(path, "tools", "RI_bb", "RI3.6-release", "ri36")
+            path_ri = os.path.join("..", "tools", "RI_bb", "RI3.6-release", "ri36")
 
     elif sys.platform == "linux":
-        path_ri = os.path.join(path, "tools", "RI_unix", "RI3.6-release", "ri36")
+        path_ri = os.path.join("..", "tools", "RI_unix", "RI3.6-release", "ri36")
 
     else:
-        path_ri = os.path.join(path, "ri36")
+        path_ri = os.path.join("..", "ri36")
 
     print("==================================")
     print(db_out)
-    print(pkls_out)
     print(sizes, boxes)
     print("==================================")
 
     create_isomorphism_database(db_out, boxes, sizes, path_ri)
 
 
-def build_substructure_database(records, path_input, path_db="../databases/substructures.sqlite", n_min=2,
-                                n_max=9, method="exhaustive"):
-    db = SubstructureDb(path_db, "", "")
-    db.create_compound_database()
-    db.close()
-
-    for i, record in enumerate(records):
-        update_substructure_database(os.path.join(path_input, record + ".xml"), path_db, n_min, n_max, method=method)
-
-    db = SubstructureDb(path_db, "", "")
-    db.create_indexes()
-    db.close()
-
-
-def build_structures(accuracy=1, heavy_atoms=range(2, 9), max_valence=4, max_atoms_available=2, max_n_substructures=3,
-                     path_db_k_graphs="../databases/k_graphs.sqlite",
-                     path_pkls="../databases/pkls",
-                     path_db="../databases/substructures.sqlite",
-                     path_out="results/", debug=False):
+def build_structures(max_n_substructures=3, path_db_k_graphs="../databases/k_graphs.sqlite",
+                     path_pkls="../databases/pkls", path_db="../databases/substructures.sqlite", path_out="results/"):
 
     db = SubstructureDb(path_db, path_pkls, path_db_k_graphs)
 
@@ -70,25 +50,17 @@ def build_structures(accuracy=1, heavy_atoms=range(2, 9), max_valence=4, max_ato
         fn_out = os.path.join(path_out, "{}.smi".format(record[0]))
 
         # build & write structures
-        build(list(record[3:9]), record[1], fn_out, heavy_atoms, max_valence, accuracy, max_atoms_available,
-              max_n_substructures, debug=debug, path_connectivity_db=path_db_k_graphs, path_pkls=path_pkls, path_substructure_db=path_db)
+        build(list(record[3:9]), record[1], max_n_substructures, fn_out, path_connectivity_db=path_db_k_graphs,
+              path_substructure_db=path_db, out_mode="w", ppm=None, processes=None, table_name="substructures",
+              prescribed_mass=None)
 
-        # write figures to svg files
-        with open(fn_out) as smiles:
-            temp_fn = tempfile.NamedTemporaryFile(mode="w", delete=False)
-            for s in set(smiles.readlines()):
-                temp_fn.write(s)
-
-            subprocess.Popen(["obabel", "-ismi", os.path.join(path_out, temp_fn.name), "-osvg", "-O",
-                              os.path.join(path_out, "figures", "%s.svg" % (record[0]))],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            temp_fn.seek(0)
-            temp_fn.close()
     db.close()
 
 
 if __name__ == "__main__":
     build_graph_isomorphism_database()
-    build_substructure_database(["HMDB0000001", "HMDB0000005", "HMDB0000008", "HMDB0000122"], "input")
+
+    hmdbs = [os.path.join("input", h + ".xml") for h in ["HMDB0000001", "HMDB0000005", "HMDB0000008", "HMDB0000122"]]
+    create_substructure_database(hmdbs, "../databases/substructures.sqlite", 2, 9)
+
     build_structures()
